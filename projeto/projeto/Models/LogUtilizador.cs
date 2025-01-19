fa169dea1f0f4a80b8b68c2738562afed45c185a
Miguel Pinto <127101134+202200964@users.noreply.github.com>
2025-01-19 09:29:16 +0000

    View ForgotPassword, VerificationCode e ResetPassword
    .

---
+++ projeto/projeto/Models/LogUtilizador.cs
@@ -0,0 +1,10 @@
+﻿namespace projeto.Models
+{
+    public class LogUtilizador
+    {
+        public int LogId { get; set; } 
+        public DateTime LogDataLogin { get; set; } = DateTime.Now;
+        public Utilizador User { get; set; }
+        public string LogMessage { get; set; }
+    }
+}
